abe5c4b39654633562f8ebbf0e7c2e02e4425cef
Alex Valyshev <avalyshev@mail.ru>
2020-06-17 15:12:11 +0300

    База для проекта

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
+  <div class="page">
+    <header>this is header</header>
     <nuxt/>
   </div>
 </template>
 
 <style>
-html {
-
+body {
+  width: 100%;
+  margin: 0;
+  background: #f2f2f2;
+  padding: 126px 0;
+  font-family: 'Open Sans', 'Lato', 'Arial', sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
 }
 
+.page {
+  max-width: 1180px;
+  height: 771px;  /* потом нужно убрать, сформируется по высоте контента*/
+  border: 1px solid red; /* чтобы видеть границы*/
+  margin: 0 auto;
+  background: #f7f8f9;
+  border-radius: 8px;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.2);
+}
 
 </style>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: '{{ name }}',
+    title: 'jungaroo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -12,6 +12,12 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
+  },
+  exports: {
+    css: [
+      '~/assets/css/fonts.css',
+      '~/assets/css/main.css'
+    ],
   },
   /*
   ** Customize the progress bar color
