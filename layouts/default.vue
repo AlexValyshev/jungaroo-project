a6828cd74ee65e29f1391ae3a3fcff3a3448f766
Alex Valyshev <avalyshev@mail.ru>
2020-06-21 16:35:28 +0300

    feat: Сделать блок header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="page page_theme_light">
-    <Header />
-    <SideBar class="sidebar"/>
     <nuxt/>
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header/Header.vue';
-import SideBar from "~/components/SideBar.vue";
-export default {
-  components: {
-    Header,
-    SideBar
-  }
-};
+
 </script>
 
 <style>
 .page {
   width: 100%;
   height: 100%;
-  border: 1px solid red; /* чтобы видеть границы*/
+  /* border: 1px solid red; чтобы видеть границы */
   border-radius: 8px;
   font-family: 'Open Sans', 'Lato', 'Arial', sans-serif;
   -moz-osx-font-smoothing: grayscale;
@@ -35,14 +26,4 @@ export default {
   box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.2);
 }
 
-.header {
-  position: relative;
-  z-index: 1;
-}
-
-.sidebar {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
 </style>
